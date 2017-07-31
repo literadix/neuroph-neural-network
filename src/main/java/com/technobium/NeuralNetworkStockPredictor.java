bd0ac519c6d23acdcbeecc82a99bf088846b6545
Maciej Bednarz <maciej.andreas.bednarz@hacon.de>
2017-07-31 15:35:10 +0200

    Aktuelle Änderungen

--- src/main/java/com/technobium/NeuralNetworkStockPredictor.java
+++ src/main/java/com/technobium/NeuralNetworkStockPredictor.java
@@ -16,6 +16,9 @@ import org.neuroph.core.learning.SupervisedLearning;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.learning.BackPropagation;
 
+// https://stackoverflow.com/questions/16143266/get-bitcoin-historical-data
+// http://api.bitcoincharts.com/v1/csv/
+
 public class NeuralNetworkStockPredictor {
 
 	private int slidingWindowSize;
@@ -28,7 +31,7 @@ public class NeuralNetworkStockPredictor {
 
 	public static void main(String[] args) throws IOException {
 
-		NeuralNetworkStockPredictor predictor = new NeuralNetworkStockPredictor(5, "input/rawTrainingData.csv");
+		NeuralNetworkStockPredictor predictor = new NeuralNetworkStockPredictor(10, "input/rawTrainingData.csv");
 		predictor.prepareData();
 
 		System.out.println("Training starting");
@@ -44,9 +47,12 @@ public class NeuralNetworkStockPredictor {
 	}
 
 	void prepareData() throws IOException {
+
 		BufferedReader reader = new BufferedReader(new FileReader(rawDataFilePath));
 		// Find the minimum and maximum values - needed for normalization
+
 		try {
+
 			String line;
 			while ((line = reader.readLine()) != null) {
 				String[] tokens = line.split(",");
@@ -58,6 +64,7 @@ public class NeuralNetworkStockPredictor {
 					min = crtValue;
 				}
 			}
+
 		} finally {
 			reader.close();
 		}
@@ -70,17 +77,20 @@ public class NeuralNetworkStockPredictor {
 		try {
 			String line;
 			while ((line = reader.readLine()) != null) {
+
 				double crtValue = Double.valueOf(line.split(",")[1]);
+
 				// Normalize values and add it to the queue
 				double normalizedValue = normalizeValue(crtValue);
 				valuesQueue.add(normalizedValue);
 
 				if (valuesQueue.size() == slidingWindowSize + 1) {
+
 					String valueLine = valuesQueue.toString().replaceAll("\\[|\\]", "");
 					writer.write(valueLine);
 					writer.newLine();
-					// Remove the first element in queue to make place for a new
-					// one
+
+					// Remove the first element in queue to make place for a new one
 					valuesQueue.removeFirst();
 				}
 			}
@@ -90,32 +100,41 @@ public class NeuralNetworkStockPredictor {
 		}
 	}
 
-	double normalizeValue(double input) {
+	private double normalizeValue(double input) {
 		return (input - min) / (max - min) * 0.8 + 0.1;
 	}
 
-	double deNormalizeValue(double input) {
+	private double deNormalizeValue(double input) {
 		return min + (input - 0.1) * (max - min) / 0.8;
 	}
 
-	void trainNetwork() throws IOException {
+	private void trainNetwork() throws IOException {
 
 		NeuralNetwork<BackPropagation> neuralNetwork = new MultiLayerPerceptron(slidingWindowSize,
-				2 * slidingWindowSize + 1, 1);
+				2 * slidingWindowSize + 1, slidingWindowSize, 1);
 
 		int maxIterations = 1000;
 		double learningRate = 0.5;
 		double maxError = 0.00001;
+        double minErrorChange = maxError / 2;
 
-		SupervisedLearning learningRule = neuralNetwork.getLearningRule();
+		final SupervisedLearning learningRule = neuralNetwork.getLearningRule();
 		learningRule.setMaxError(maxError);
 		learningRule.setLearningRate(learningRate);
 		learningRule.setMaxIterations(maxIterations);
+        learningRule.setMinErrorChange(minErrorChange);
 		learningRule.addListener(new LearningEventListener() {
 			public void handleLearningEvent(LearningEvent learningEvent) {
-				SupervisedLearning rule = (SupervisedLearning) learningEvent.getSource();
-				System.out.println("Network error for interation " + rule.getCurrentIteration() + ": "
-						+ rule.getTotalNetworkError());
+
+			    if (learningRule.getCurrentIteration() % 100 == 0){
+
+                    SupervisedLearning rule = (SupervisedLearning) learningEvent.getSource();
+
+                    System.out.println(String.format("Network error for iteration  %d : %.6f%%", rule.getCurrentIteration(), rule.getTotalNetworkError() * 100));
+
+                }
+
+
 			}
 		});
 
@@ -124,19 +143,22 @@ public class NeuralNetworkStockPredictor {
 		neuralNetwork.save(neuralNetworkModelFilePath);
 	}
 
-	DataSet loadTraininigData(String filePath) throws IOException {
+	private DataSet loadTraininigData(String filePath) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(filePath));
 		DataSet trainingSet = new DataSet(slidingWindowSize, 1);
 
 		try {
 			String line;
 			while ((line = reader.readLine()) != null) {
+
 				String[] tokens = line.split(",");
 
 				double trainValues[] = new double[slidingWindowSize];
+
 				for (int i = 0; i < slidingWindowSize; i++) {
 					trainValues[i] = Double.valueOf(tokens[i]);
 				}
+
 				double expectedValue[] = new double[] { Double.valueOf(tokens[slidingWindowSize]) };
 				trainingSet.addRow(new DataSetRow(trainValues, expectedValue));
 			}
@@ -149,13 +171,24 @@ public class NeuralNetworkStockPredictor {
 	void testNetwork() {
 
 		NeuralNetwork neuralNetwork = NeuralNetwork.createFromFile(neuralNetworkModelFilePath);
-		neuralNetwork.setInput(normalizeValue(2056.15), normalizeValue(2061.02), normalizeValue(2086.24),
-				normalizeValue(2067.89), normalizeValue(2059.69));
+		neuralNetwork.setInput(
+		        normalizeValue(2089.27),
+                normalizeValue(2108.1),
+                normalizeValue(2104.42),
+                normalizeValue(2091.5),
+                normalizeValue(2061.05),
+                normalizeValue(2056.15),
+                normalizeValue(2061.02),
+                normalizeValue(2086.24),
+                normalizeValue(2067.89),
+                normalizeValue(2059.69)
+        );
 
 		neuralNetwork.calculate();
 		double[] networkOutput = neuralNetwork.getOutput();
 		System.out.println(String.format("Expected  value  : %.2f", 2066.96));
         System.out.println(String.format("Predicted value  : %.2f", deNormalizeValue(networkOutput[0])));
+        System.out.println(String.format("Predicted error  : %.2f%%", Math.abs((2066.96 - deNormalizeValue(networkOutput[0])) / 2066.96 * 100.0)));
 
 	}
 }
